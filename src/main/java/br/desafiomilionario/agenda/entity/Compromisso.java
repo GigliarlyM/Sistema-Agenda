5806270d1ebf83ad46efcc41b712b111dc659117
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2025-05-16 16:28:23 -0300

    add Spring Boot JPA (persistencia) add 

--- src/main/java/br/desafiomilionario/agenda/entity/Compromisso.java
+++ src/main/java/br/desafiomilionario/agenda/entity/Compromisso.java
@@ -1,4 +1,19 @@
 package br.desafiomilionario.agenda.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_compromisso")
+@Data
 public class Compromisso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String titulo;
+    Date dataHora;
+    String local;
+    Boolean status;
 }
