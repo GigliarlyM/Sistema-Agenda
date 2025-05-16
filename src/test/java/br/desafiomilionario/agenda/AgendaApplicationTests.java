1732897259098199ce1836098763024194586470
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2025-05-16 14:59:07 -0300

    Initial files

---
+++ src/test/java/br/desafiomilionario/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.desafiomilionario.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
